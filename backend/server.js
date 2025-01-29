958369fb6309333f164f431197eccf4254e12dc6
WahabWorks <abdulwahabamar979@gmail.com>
2025-01-30 00:21:32 +0500

    Connected to MongoDB localhost

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import colors from 'colors'
+import connectDB from './config/db.js';
 
-const app = express();
+// MongoDB Connection
+connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("<h1>dfhuieukkskwqjijwi</h1>")
+const server = express();
+
+server.get("/",(req,res)=>{
+    res.send("<h1>Creating server (express)</h1>")
 })
 
 const PORT = 8080;
-app.listen(PORT,()=>{console.log(`Server is Running at PORT ${PORT}`.bgBlue)});
+server.listen(PORT,"Localhost",()=>{console.log(`Server is Running at PORT ${PORT}`.bgBlue)});
