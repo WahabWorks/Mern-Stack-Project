e51d1c6602082da36ae948aa09b191ccca6012b2
WahabWorks <abdulwahabamar979@gmail.com>
2025-01-30 01:39:43 +0500

    UserModel

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         const con =await mongoose.connect('mongodb://localhost:27017/shopwaver');
-        console.log(`Connected to MongoDB ${con.connection.host}`.bgGreen);
+        console.log(`Connected to MongoDB ${con.connection.host}`.bgGreen );
     
     } catch (error) {
         console.log(`MongoDB Connection Error ${error}`);
---
+++ backend/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength: [8, 'Name must be at least 3 characters long'], // Custom message for minlength
+        maxlength: [20, 'Name cannot exceed 50 characters'], // Custom message for maxlength
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        lowercase:true
+    },
+    password:{
+        type: String,
+        required:true,
+        minlength: 8
+    },
+     role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user'
+     },
+     createdAt: {
+      type: Date,
+      default: Date.now
+     },
+     lastLogin: {
+      type: Date
+     }
+},
+{timestamps:true}
+)
+
+export default mongoose.Model("User",userSchema)
